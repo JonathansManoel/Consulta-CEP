f34468bbfc6fc67c25b01b857299bdcee52ccf6f
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-03-16 02:07:58 -0300

    Implementando Testes. Em construção.

---
+++ tests_consulta-CEP/test_consulta_CEP.py
@@ -0,0 +1,25 @@
+import pytest
+import consulta_CEP
+from consulta_CEP import consulta_cep
+import requests_mock
+from unittest import mock
+
+
+def test_func_consulta_cep():
+    mock.patch('consulta_CEP.consulta_cep', return_value=consulta_cep)
+
+
+
+# Tentando encontrar e aprender como realizar testes!
+# def test_consulta_cep_status_code():
+#     consulta_cep.status_code == 200
+
+
+# class TesteConsutla(consulta_cep):
+#     @requests_mock.Mocker()
+#     def test_status_code_200(self, r_mock):
+#         url = 'https://viacep.com.br/ws/01001000/json/'
+#         r_mock.get(url, status_code=200)
+#         site = consulta_CEP.consulta_cep.r(url=url)
+#         site.consulta_cep()
+#         self.assertEqual(200, site.status)
